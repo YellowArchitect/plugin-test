2421de5769c4c83749e78d40d60e0ee7047e62ab
Ben Schmidt <54752448+YellowArchitect@users.noreply.github.com>
2020-04-01 17:41:51 -0400

    WORK PLZ
    Trying to set things up

---
+++ TestPlugin/src/main/java/io/github/yellowarchitect/testplugin/Testplugin.java
@@ -0,0 +1,15 @@
+/**
+ * This is a cool plugin
+ */
+package io.github.yellowarchitect.testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Benjamin Schmidt
+ * @version 0.1.1
+ *
+ */
+public class Testplugin extends JavaPlugin {
+
+}
